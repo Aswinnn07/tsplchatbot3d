4a0f39746ae1e21620e02520fe1c4b1f1142371f
aswin kmm <aswin.km@triassicsolutions.com>
2025-01-02 11:09:16 +0530

    initial files added

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,19 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import { useThree } from "@react-three/fiber";
 
 export const Experience = () => {
+  const texture = useTexture("./textures/youtubeBackground.jpg");
+  const viewport = useThree((state) => state.viewport);
+
   return (
     <>
       <OrbitControls />
+      <Avatar position={[0, -6.5, 5]} scale={4} />
+      <Environment preset="sunset" /> 
       <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+        <planeGeometry args={[viewport.width, viewport.height]}/>
+        <meshBasicMaterial map={texture}/>
       </mesh>
     </>
   );
